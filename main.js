178e47ff4c548ff07466599094c3052b00781b96
C4erries <85022477+C4erries@users.noreply.github.com>
2024-12-02 22:07:48 +0300

    project

--- main.js
+++ main.js
@@ -1,15 +1,41 @@
 
 $(document).ready(()=>{
+    $('form').on('submit', function(event) {
+        event.preventDefault();
+        var $form = $(this);
+        var $inputs = $form.find('input, button, textarea');
+        var serializedData = $form.serialize();
+
+        $inputs.prop('disabled', true);
+
+        $.ajax({
+            url: "https://formcarry.com/s/EpNtk1D-eg6",
+            type: 'POST',
+            data: serializedData,
+            success: function(response) {
+            },
+            complete: function() {
+                $inputs.prop('disabled', false);
+            }
+        });
+    });
+
+
+
+    const $navMenu = $("#mobileMenu");
+    $("#navbar-toggle").click(function(event){
+        $navMenu.toggle("slide", { direction: "down" }, 600)
+    });
     const updateSliderCounter =(current, total)=>{
         current = current < 10 ? "0" + current : current;
         total = total < 10 ? "0" + total : total;
         $(".slick-review-counter").text(current+"/"+total)
+
     };
 
     const $slickReview = $("#slick-review")
 
     $slickReview.on("init", function (ev, slick){
-        console.log(slick)
         updateSliderCounter(slick.slickCurrentSlide() + 1, slick.slideCount);
     })
 
@@ -26,7 +52,6 @@ $(document).ready(()=>{
 
     $(".review-arrow-prev").click(function (){
         $("#slick-review").slick("slickPrev")
-        console.log("213123")
     })
     $(".review-arrow-next").click(function (){
         $("#slick-review").slick("slickNext")
@@ -37,4 +62,82 @@ $(document).ready(()=>{
         t.toggleClass("panel_close")
         t.children(".panel-body").slideToggle(400)
     })
+    $('#slick-customers-first').slick({
+        infinite: true,
+        speed: 600,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        nextArrow: ``,
+        prevArrow: ``,
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 5,
+                    slidesToScroll: 1,
+                    infinite: true,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
+    $('#slick-customers-second').slick({
+        infinite: true,
+        speed: 600,
+        autoplay: true,
+        autoplaySpeed: 3250,
+        nextArrow: ``,
+        prevArrow: ``,
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 5,
+                    slidesToScroll: 1,
+                    infinite: true,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
+
+
+
 })
+
